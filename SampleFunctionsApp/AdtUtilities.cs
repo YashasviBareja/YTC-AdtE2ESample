8c9e230c1564531d3802d3410fd60c63917b1bbd
Yashasvi <SESA650051@se.com>
2022-11-11 18:50:43 +0530

    Add project files.

---
+++ SampleFunctionsApp/AdtUtilities.cs
@@ -0,0 +1,86 @@
+﻿using Azure;
+using Azure.DigitalTwins.Core;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SampleFunctionsApp
+{
+    internal static class AdtUtilities
+    {
+        public static async Task<List<string>> FindParentAsync(DigitalTwinsClient client, string child, string relname, ILogger log)
+        {
+            List<string> allTargetIds = new List<string>();
+
+            // Find parent using incoming relationships
+            try
+            {
+               // AsyncPageable<IncomingRelationship> rels = client.GetIncomingRelationshipsAsync(child);
+
+                //await foreach (IncomingRelationship ie in rels)
+                //{
+                //    if (ie.RelationshipName == relname)
+                //        return (ie.SourceId);
+                //} 
+                
+                AsyncPageable<BasicRelationship> allRelationships = client.GetRelationshipsAsync<BasicRelationship>(child);
+
+
+                await foreach (BasicRelationship relationship in allRelationships)
+                {
+                    if (relationship.Name == relname)
+                        allTargetIds.Add(relationship.TargetId);
+
+                    // return (relationship.TargetId);
+
+                }
+            }
+            catch (RequestFailedException exc)
+            {
+                log.LogInformation($"*** Error in retrieving parent:{exc.Status}:{exc.Message}");
+            }
+            return allTargetIds;
+        }
+
+        public static async Task<string> FindParentByQueryAsync(DigitalTwinsClient client, string childId, ILogger log)
+        {
+            string query = "SELECT Parent " +
+                           "FROM digitaltwins Parent " +
+                           "JOIN Child RELATED Parent.contains " +
+                           $"WHERE Child.$dtId = '{childId}'";
+            log.LogInformation($"Query: {query}");
+
+            try
+            {
+                AsyncPageable<BasicDigitalTwin> twins = client.QueryAsync<BasicDigitalTwin>(query);
+                await foreach (BasicDigitalTwin twin in twins)
+                {
+                    return twin.Id;
+                }
+                log.LogWarning($"*** No parent found");
+            }
+            catch (RequestFailedException exc)
+            {
+                log.LogError($"*** Error in retrieving parent:{exc.Status}/{exc.Message}");
+            }
+            return null;
+        }
+
+        public static async Task UpdateTwinPropertyAsync(DigitalTwinsClient client, string twinId, string propertyPath, object value, ILogger log)
+        {
+            // If the twin does not exist, this will log an error
+            try
+            {
+                var updateTwinData = new JsonPatchDocument();
+                updateTwinData.AppendReplace(propertyPath, value);
+
+                log.LogInformation($"UpdateTwinPropertyAsync sending {updateTwinData}");
+                await client.UpdateDigitalTwinAsync(twinId, updateTwinData);
+            }
+            catch (RequestFailedException exc)
+            {
+                log.LogInformation($"*** Error:{exc.Status}/{exc.Message}");
+            }
+        }
+    }
+}
